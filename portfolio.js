bfc129d631641c4c8ce96e840569fb3ed54a97b8
Raghav Babbar <raghavbabbar12345@gmail.com>
2024-03-17 23:35:11 +0530

    sorted css and js

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+var typed = new Typed("#name", {strings: ["Developer", "Student","Raghav Babbar"],
+        typeSpeed : 40,
+        backSpeed : 40,
+        backDelay : 1500,
+        showCursor : false
+        })
