b22cf081bef3b385e2a2f531054f2d4211b53e59
Raghav Babbar <raghavbabbar12345@gmail.com>
2024-03-19 12:32:15 +0530

    animations and contact info

--- portfolio.js
+++ portfolio.js
@@ -4,3 +4,34 @@ var typed = new Typed("#name", {strings: ["Developer", "Student","Raghav Babbar"
         backDelay : 1500,
         showCursor : false
         })
+
+const fade_observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if (entry.isIntersecting){
+            entry.target.classList.add("fadein_show")
+        }
+        else{
+            entry.target.classList.remove("fadein_show")
+        }
+    })
+})
+
+const bottotop_observer = new IntersectionObserver((entries)=>{
+        entries.forEach((entry)=>{
+            if (entry.isIntersecting){
+                entry.target.classList.add("bottotop_show")
+            }
+            else{
+                entry.target.classList.remove("bottotop_show")
+            }
+        })
+    })
+
+const fadeinelements = document.querySelectorAll(".fadein")
+fadeinelements.forEach((el)=>{
+    fade_observer.observe(el)
+})
+const bottotopelements = document.querySelectorAll(".bottotop")
+bottotopelements.forEach((el)=>{
+    bottotop_observer.observe(el)
+})
