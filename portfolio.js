a97777e0da1846a76d401987570a7c7f2ab79755
Raghav Babbar <raghavbabbar12345@gmail.com>
2024-03-28 00:25:31 +0530

    fixed cursor

--- portfolio.js
+++ portfolio.js
@@ -36,23 +36,34 @@ bottotopelements.forEach((el)=>{
     bottotop_observer.observe(el)
 })
 
+
+
+let details = navigator.userAgent; 
+let regexp = /android|iphone|kindle|ipad/i; 
+let isMobileDevice = regexp.test(details); 
 const cursor_dot = document.getElementById("cursor_dot");
 const cursor_outline = document.getElementById("cursor_outline");
 
-window.addEventListener("mousemove", function (e) {
-    const posx = e.clientX;
-    const posy = e.clientY;
-    cursor_dot.style.left = posx + "px";
-    cursor_dot.style.top = posy + "px";
-    cursor_outline.animate({left : posx + "px", top : posy + "px"}, {duration : 400, fill : "forwards"});
-})
-document.documentElement.addEventListener('mouseleave', function (e) {
-    cursor_outline.animate({opacity : 0}, {duration : 500, fill : "forwards"});
-    cursor_dot.animate({opacity : 0}, {duration : 500, fill : "forwards"});
-})
-document.documentElement.addEventListener('mouseenter', function (e) {
-    cursor_dot.style.opacity= "1";
-    cursor_dot.style.opacity= "1";
-    cursor_outline.animate({opacity : 1}, {duration : 500, fill : "forwards"});
-    cursor_dot.animate({opacity : 1}, {duration : 500, fill : "forwards"});
-})
+if (!isMobileDevice) { 
+    window.addEventListener("mousemove", function (e) {
+        const posx = e.clientX;
+        const posy = e.clientY;
+        cursor_dot.style.left = posx + "px";
+        cursor_dot.style.top = posy + "px";
+        cursor_outline.animate({left : posx + "px", top : posy + "px"}, {duration : 400, fill : "forwards"});
+    })
+    document.documentElement.addEventListener('mouseleave', function (e) {
+        cursor_outline.animate({opacity : 0}, {duration : 500, fill : "forwards"});
+        cursor_dot.animate({opacity : 0}, {duration : 500, fill : "forwards"});
+    })
+    document.documentElement.addEventListener('mouseenter', function (e) {
+        cursor_dot.style.opacity= "1";
+        cursor_dot.style.opacity= "1";
+        cursor_outline.animate({opacity : 1}, {duration : 500, fill : "forwards"});
+        cursor_dot.animate({opacity : 1}, {duration : 500, fill : "forwards"});
+    })
+} 
+else { 
+	cursor_dot.remove()
+    cursor_outline.remove()
+}
