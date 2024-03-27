56e63d4bf767087b150b02da89b7701dac32e8a4
Raghav Babbar <raghavbabbar12345@gmail.com>
2024-03-27 18:16:57 +0530

    custom cursor

--- portfolio.js
+++ portfolio.js
@@ -35,3 +35,24 @@ const bottotopelements = document.querySelectorAll(".bottotop")
 bottotopelements.forEach((el)=>{
     bottotop_observer.observe(el)
 })
+
+const cursor_dot = document.getElementById("cursor_dot");
+const cursor_outline = document.getElementById("cursor_outline");
+
+window.addEventListener("mousemove", function (e) {
+    const posx = e.clientX;
+    const posy = e.clientY;
+    cursor_dot.style.left = posx + "px";
+    cursor_dot.style.top = posy + "px";
+    cursor_outline.animate({left : posx + "px", top : posy + "px"}, {duration : 400, fill : "forwards"});
+})
+document.documentElement.addEventListener('mouseleave', function (e) {
+    cursor_outline.animate({opacity : 0}, {duration : 500, fill : "forwards"});
+    cursor_dot.animate({opacity : 0}, {duration : 500, fill : "forwards"});
+})
+document.documentElement.addEventListener('mouseenter', function (e) {
+    cursor_dot.style.opacity= "1";
+    cursor_dot.style.opacity= "1";
+    cursor_outline.animate({opacity : 1}, {duration : 500, fill : "forwards"});
+    cursor_dot.animate({opacity : 1}, {duration : 500, fill : "forwards"});
+})
